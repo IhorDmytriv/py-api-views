7c02aae5ad0ef86e6f206365dfe013c783b36553
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-03 18:53:12 +0200

    Create views for Actor model use GenericAPIView and mixins

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from cinema.views import MovieList, MovieDetail, GenreList, GenreDetail
+from cinema.views import (
+    MovieList,
+    MovieDetail,
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail
+)
 
 urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("movies/", MovieList.as_view(), name="movie-list"),
     path("movies/<int:pk>/", MovieDetail.as_view(), name="movie-detail"),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,16 @@
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, mixins
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre
-from cinema.serializers import MovieSerializer, GenreSerializer
+from cinema.models import Movie, Genre, Actor
+from cinema.serializers import (
+    MovieSerializer,
+    GenreSerializer,
+    ActorSerializer
+)
 
 
 class GenreList(APIView):
@@ -47,6 +52,43 @@ class GenreDetail(APIView):
         genre = self.get_object(pk=pk)
         genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ActorList(
+    GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs) -> Response:
+        return self.create(request, *args, **kwargs)
+
+
+class ActorDetail(
+    GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs) -> Response:
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs) -> Response:
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs) -> Response:
+        return self.destroy(request, *args, **kwargs)
 
 
 class MovieList(APIView):
